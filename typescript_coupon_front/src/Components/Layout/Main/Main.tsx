183b37fdbd9e041a64652fb12dbb2415bd11ed6c
moradi12 <tamirmoradi@gmail.com>
2024-11-12 11:17:29 +0200

    fixing main component

--- typescript_coupon_front/src/Components/Layout/Main/Main.tsx
+++ typescript_coupon_front/src/Components/Layout/Main/Main.tsx
@@ -1,18 +1,8 @@
 import { useEffect, useState } from "react";
 import { Coupon } from "../../Models/Coupon";
 import { getAllCoupons } from "../../Utils/CouponsCommands";
-//todo : finish 
 
 export function Main(): JSX.Element {
-  return (
-    <div className="Main">
-      <h1></h1>
-      <AllCoupons />
-    </div>
-  );
-}
-
-export function AllCoupons(): JSX.Element {
   const [coupons, setCoupons] = useState<Coupon[]>([]);
   const [error, setError] = useState<string | null>(null);
 
@@ -28,22 +18,23 @@ export function AllCoupons(): JSX.Element {
   }, []);
 
   return (
-    <div className="AllCoupons">
-      <h2>All Coupons</h2>
-      {error && <p className="error-message">{error}</p>}{" "}
-      
-      <div className="coupon-list">
-        {coupons.length > 0 ? (
-          coupons.map((coupon) => (
-            <div key={coupon.id} className="coupon-card">
-              <p>{coupon.description}</p>
-              <p>Price: ${coupon.price}</p>
-              {/* <p>Available: {coupon.isAvailable ? "Yes" : "No"}</p> */}
-            </div>
-          ))
-        ) : (
-          <p>No coupons available.</p>
-        )}
+    <div className="Main">
+      <div className="AllCoupons">
+        <h2>All Coupons</h2>
+        {error && <p className="error-message">{error}</p>}{" "}
+        <div className="coupon-list">
+          {coupons.length > 0 ? (
+            coupons.map((coupon) => (
+              <div key={coupon.id} className="coupon-card">
+                <p>{coupon.description}</p>
+                <p>Price: ${coupon.price}</p>
+                {/* <p>Available: {coupon.isAvailable ? "Yes" : "No"}</p> */}
+              </div>
+            ))
+          ) : (
+            <p>No coupons available.</p>
+          )}
+        </div>
       </div>
     </div>
   );
